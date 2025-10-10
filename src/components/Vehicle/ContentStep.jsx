65b2f3198cdc5ba5c8b6dad3f6bfaa07999d4014
handuong205 <htduong2005@gmail.com>
2025-10-10 20:02:27 +0700

    feat: Add Vehicle

--- src/components/Vehicle/ContentStep.jsx
+++ src/components/Vehicle/ContentStep.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
-import StepperContext from '../../contexts/Vehicle/StepperProvider'
+import StepperContext from '../../contexts/Vehicle/StepperProvider';
 import { useContext } from 'react';
+import Step1 from './Contents/Step1';
+import Step2 from './Contents/Step2';
+import Step3 from './Contents/Step3';
+import Step4 from './Contents/Step4';
 
 function ContentStep() {
     const {currentStep} = useContext(StepperContext);
@@ -9,9 +13,13 @@ function ContentStep() {
     const handleRenderContet = () => {
         switch(currentStep){
             case 1:
-                return <div>Step 1</div>   
+                return <div><Step1/></div>  
             case 2:  
-                return <div>Step 2</div>       
+                return <div><Step2/></div>
+            case 3:  
+                return <div><Step3/></div>       
+            case 4:  
+                return <div><Step4/></div>
         }
     };
   return (
