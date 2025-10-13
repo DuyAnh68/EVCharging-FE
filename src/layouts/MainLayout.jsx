8715dd051d310ac527790d3bb5657a6d3ee92c01
Duy <hugoquan9@gmail.com>
2025-10-14 01:42:46 +0700

    feat: simulate login

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,17 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Layout/Header";
-import { useNavigate } from "react-router-dom";
 
 const MainLayout = () => {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    navigate("/auth");
-  };
-
   return (
     <div className="min-h-screen bg-gray-50">
-      <Header isAuthenticated={true} handleLogout={handleLogout} />
+      <Header />
       <div className="pt-16">
         <div className="min-w-screen mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <main>
--- src/pages/Home/ChargingStationMap.jsx
+++ src/pages/Home/ChargingStationMap.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import TestAuth from "../../components/TestAuth";
 
 function ChargingStationMap() {
-  return <div>ChargingStationMap</div>;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Trang chủ - Bản đồ trạm sạc</h1>
+    </div>
+  );
 }
 
 export default ChargingStationMap;
