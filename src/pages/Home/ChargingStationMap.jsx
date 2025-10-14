2025c4a937df6bfbbcb326b0506d2c0bae6c4172
Duy <hugoquan9@gmail.com>
2025-10-14 07:37:45 +0700

    resolve conflict

--- src/pages/Home/ChargingStationMap.jsx
+++ src/pages/Home/ChargingStationMap.jsx
@@ -1,24 +1,5 @@
-import React from "react";
-import SearchForm from "../../components/SearchForm";
-import ChargingStationMapComponent from "../../components/Map/ChargingStationMap";
-
 function ChargingStationMap() {
-  return (
-    <>
-      <div className="title text-center">
-        <h1 className="text-2xl font-bold mb-4">Tìm trạm sạc gần bạn</h1>
-        <p>
-          Khám phá và đặt chỗ tại hàng nghìn trạm sạc xe điện trên toàn quốc
-        </p>
-      </div>
-      <div className="searchSpace">
-        <SearchForm />
-      </div>
-      <div className="map mt-8">
-        <ChargingStationMapComponent />
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default ChargingStationMap;
